31c826fcabebecbbfdbc509c6871675c519f57bc
Daniel Dombrowsky <ddombrow@gmail.com>
2018-03-31 11:22:58 -0400

    Hello world graphql server.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
-const { json } = require("micro");
+const { json, send } = require("micro");
 const pino = require("pino")();
-module.exports = async (req, res) => {
-	const body = await json(req);
+const { graphql, buildSchema } = require("graphql");
 
-	res.setHeader("Content-Type", "application/json");
-	return {};
+module.exports = async (req, res) => {
+	try {
+		const body = await json(req);
+
+		const schemaStr = `
+	type Query {
+		hello: String
+	}
+	`;
+		const schema = buildSchema(schemaStr);
+
+		const root = {
+			hello: () => {
+				pino.info("hey.");
+				return "Hello world!";
+			}
+		};
+
+		let result;
+
+		result = await graphql(schema, body.query, root);
+		res.setHeader("Content-Type", "application/json");
+		return result;
+	} catch (err) {
+		result = {};
+		pino.error(err);
+		send(res, 500);
+	}
 };
